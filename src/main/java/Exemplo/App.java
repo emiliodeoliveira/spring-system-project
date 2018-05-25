9abc0b81aecb6a4efd0b717e5136f8db4c87f8ef
Emilio de Oliveira <emilio.oliveira@safelight.info>
2018-05-25 15:32:06 -0300

    Closes #2, closes #3 Executado com Mavem e criado teste com JUnit

--- src/main/java/Exemplo/App.java
+++ src/main/java/Exemplo/App.java
@@ -1,5 +1,7 @@
 package Exemplo;
 
+
+
 /**
  * Hello world!
  *
@@ -8,6 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( getGreeting() );
     }
+
+	/**
+	 * @return
+	 */
+	public static String getGreeting() {
+		return "Hello world!";
+	}
 }
--- src/main/java/Exemplo/Exemplo.java
+++
@@ -1,8 +0,0 @@
-package Exemplo;
-
-public class Exemplo {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-
-}
