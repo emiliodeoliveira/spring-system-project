fde7808641276d852ebf7efb2c7a3deeeefa164c
Emilio de Oliveira <emilio.oliveira@safelight.info>
2018-05-25 13:10:24 -0300

    #1 Criado Hello World

---
+++ src/main/java/Exemplo/App.java
@@ -0,0 +1,13 @@
+package Exemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
